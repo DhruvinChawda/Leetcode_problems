eddf5c7aa03ec4291cd803fdb98c266f421e0edb
DhruvinChawda <dhruvinchawda@gmail.com>
2024-08-04 01:09:07 +0530

    leetcode

--- leetcode2418.cpp
+++ leetcode2418.cpp
@@ -7,12 +7,10 @@ public:
             vec.emplace_back(heights[i], names[i]);
         }
 
-        // Sort the vector in descending order based on the heights
         sort(vec.begin(), vec.end(), [](const auto &a, const auto &b) {
             return a.first > b.first;
         });
 
-        // Extract the sorted names
         vector<string> sortedNames;
         for (const auto &p : vec) {
             sortedNames.push_back(p.second);
